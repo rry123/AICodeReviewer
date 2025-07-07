6dde32290a44ef319f5deb5ce378091d06821199
rry123 <rajy25122@gmail.com>
2025-07-07 20:22:34 +0530

    Some changes

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -33,7 +33,8 @@ function App() {
   async function reviewCode() {
     setLoading(true);
     try {
-      const response = await axios.post('http://localhost:3000/ai/get-review', { code });
+      // const response = await axios.post('http://localhost:3000/ai/get-review', { code });
+      const response = await axios.post('https://aicodereviewer-17eq.onrender.com//ai/get-review', { code });
       setReview(response.data);
     } catch (error) {
       console.error("Error fetching review:", error);
